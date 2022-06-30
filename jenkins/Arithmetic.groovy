703a9590c443d82e9600cec0a449ebdfe18ef557
Mithilesh Bairwa <107678068+mith1630@users.noreply.github.com>
2022-06-30 19:37:42 +0530

    Update Arithmetic.groovy

--- jenkins/Arithmetic.groovy
+++ jenkins/Arithmetic.groovy
@@ -3,22 +3,22 @@ pipeline {
      stages {
         stage('Addition') {
             steps {
-               sh "myscripts/Add.sh $a $b"
+               sh "myscripts/Add.sh $1 $2"
             }
         }
         stage('Subtraction') {
             steps {
-               sh "myscripts/Sub.sh $a $b"
+               sh "myscripts/Sub.sh $1 $2"
            }
        }
        stage('Multiplication') {
             steps {
-               sh "myscripts/Prod.sh $a $b"
+               sh "myscripts/Prod.sh $1 $2"
            }
        }
        stage('Division') {
             steps {
-               sh "myscripts/Div.sh $a $b"
+               sh "myscripts/Div.sh $1 $2"
            }
        }
     }
