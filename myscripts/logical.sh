0bd7e0ea93f69a6bf42345dd47f87bb00b511bf9
mith1630 <bairwamithalesh1999@gmail.com>
2022-06-30 15:25:21 +0530

    New

---
+++ myscripts/logical.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+
+sum=$(($1+$2))
+Multplication=$(($1*$2))
+Subtraction=$(($1-$2))
+Division=$(($1/$2))
+
+echo "Sum is : $sum"
+echo "Multplication is : $Multplication"
+echo "Subtraction is : $Subtraction"
+echo "Division is : $Division"
